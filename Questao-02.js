8db875d3ddef0b2174cb4b3fee0a7b7ee9f701b7
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-24 16:25:02 -0400

    Questão 02 concluida

---
+++ Questao-02.js
@@ -0,0 +1,9 @@
+/*
+Questão 02: Escreva uma arrow function chamada saudacao que recebe um nome 
+como parâmetro e retorna uma string de saudação. 
+*/
+
+const saudacao = (x) => {
+    return "Seja bem vindo amigo " + x ;
+};
+console.log(saudacao("nikas"));
