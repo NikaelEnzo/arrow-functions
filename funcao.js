de5f265f5eea93bac7a9a11e08b2cb8eb79e116c
NikaelEnzo <enzo.marthins@gmail.com>
2024-05-06 17:17:42 -0400

    função

---
+++ funcao.js
@@ -0,0 +1,12 @@
+/*
+Uma Função sem retorno é um procedimento 
+*/
+
+//arrow function 
+const quadrado = (x) => {
+    return x * x;
+};
+console.log(quadrado(3));
+
+const quadrado2 = (x) => x * x;
+console.log(quadrado2(4));
